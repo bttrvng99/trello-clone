f6b21148195b447b358b874721cf9735142d8351
Trung <bttrung3399@gmail.com>
2024-05-24 15:54:18 +0700

    Add dark/light mode p1

--- src/App.jsx
+++ src/App.jsx
@@ -5,9 +5,27 @@ import HomeIcon from '@mui/icons-material/Home'
 import { pink } from '@mui/material/colors'
 import Typography from '@mui/material/Typography'
 
+import {
+  useColorScheme
+} from '@mui/material/styles'
+
+function ModeToggle() {
+  const { mode, setMode } = useColorScheme()
+  return (
+    <Button
+      onClick={() => {
+        setMode(mode === 'light' ? 'dark' : 'light')
+      }}
+    >
+      {mode === 'light' ? 'Turn dark' : 'Turn light'}
+    </Button>
+  )
+}
+
 function App() {
   return (
     <>
+      <ModeToggle />
       <div>
         6524652648526
       </div>
