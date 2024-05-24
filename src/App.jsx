5bed63c3792bb13924ce4184b51aa33aa0b41f08
Trung <bttrung3399@gmail.com>
2024-05-24 15:08:25 +0700

    Add pallette/theming

--- src/App.jsx
+++ src/App.jsx
@@ -3,6 +3,7 @@ import AccessAlarmIcon from '@mui/icons-material/AccessAlarm'
 import ThreeDRotation from '@mui/icons-material/ThreeDRotation'
 import HomeIcon from '@mui/icons-material/Home'
 import { pink } from '@mui/material/colors'
+import Typography from '@mui/material/Typography'
 
 function App() {
   return (
@@ -10,9 +11,11 @@ function App() {
       <div>
         6524652648526
       </div>
+      <Typography variant='body2' color='text.secondary'>Test Typo</Typography>
       <Button variant='text'>Text</Button>
-      <Button variant='contained'>Contained</Button>
+      <Button variant='contained' color='success'>Contained</Button>
       <Button variant='outlined'>Outlined</Button>
+      <br />
       <AccessAlarmIcon></AccessAlarmIcon>
       <ThreeDRotation></ThreeDRotation>
       <HomeIcon />
