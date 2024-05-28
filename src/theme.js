8e096d37619e2403e7f47058993eb6588fe86ed4
Trung <bttrung3399@gmail.com>
2024-05-28 14:16:47 +0700

    Customize/override material UI elements

--- src/theme.js
+++ src/theme.js
@@ -22,8 +22,46 @@ const theme = extendTheme({
       }
       // spacing: (factor) => `${0.25 * factor}rem`
     }
-  }
+  },
   // ...other properties
+  components: {
+    MuiButton: {
+      styleOverrides: {
+        root: {
+          textTransform: 'none'
+        }
+      }
+    },
+    MuiOutlinedInput: {
+      styleOverrides: {
+        root: ({ theme }) => {
+          return {
+            color: theme.palette.primary.main,
+            fontSize: '0.875rem',
+            '.MuiOutlinedInput-notchedOutline': {
+              borderColor: theme.palette.primary.light
+            },
+            '&:hover': {
+              '.MuiOutlinedInput-notchedOutline': {
+                borderColor: theme.palette.primary.main
+              }
+            },
+            '& fieldset': {
+              borderWidth: '1px !important'
+            }
+          }
+        }
+      }
+    },
+    MuiInputLabel: {
+      styleOverrides: {
+        root: ({ theme }) => ({
+          color: theme.palette.primary.main,
+          fontSize: '0.875rem'
+        })
+      }
+    }
+  }
 })
 
 export default theme
